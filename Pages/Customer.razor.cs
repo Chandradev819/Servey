912a29ee34cc30150966cf87c7ec79dcf616e024
Chandradev <chandradev_ps@dellteam.com>
2022-03-30 16:49:11 +0530

    Add project files.

---
+++ Pages/Customer.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Servey.Pages
+{
+    public partial class Customer : ComponentBase
+    {
+
+    }
+}
